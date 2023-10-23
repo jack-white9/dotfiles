a86f8a0d7c00741a6340ad56c4e1b3855a63a705
Jack White <jfw793@gmail.com>
2023-10-23 17:23:43 +1100

    remove existing dotfiles for clean install

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,9 +11,16 @@ echo "- neovim"
 read -r -p "Confirm that these have been installed and continue with setup? [y/n] " input
 
 if [[ $input =~ ^[Yy]$ ]]; then
+    # Remove existing dotfile configs
+    rm "$HOME/.zshrc" "$HOME/.config/starship.toml"
+    rm -rf "$HOME/.config/nvim"
+
+    # Create symlinks for dotfile configs
     ln -s "$REPO_DIR/.zshrc" "$HOME/.zshrc"
     ln -s "$REPO_DIR/starship.toml" "$HOME/.config/starship.toml"
     ln -s "$REPO_DIR/nvim/" "$HOME/.config/nvim"
+
+    # Confirm success
     echo ""
     echo "Done."
 fi
