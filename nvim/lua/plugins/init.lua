0636a06f2e42f95038084d8d14c90065f1f99df4
Jack White <jack.white@kraken.tech>
2024-10-03 08:54:31 +1000

    install vim-prettier for formatting

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -50,6 +50,11 @@ local default_plugins = {
   },
 
   {
+    "prettier/vim-prettier",
+    lazy = false,
+  },
+
+  {
     "nvim-tree/nvim-web-devicons",
     opts = function()
       return { override = require "nvchad.icons.devicons" }
