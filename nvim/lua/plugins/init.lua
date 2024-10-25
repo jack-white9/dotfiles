6dc008bce1373eadc41e025ad8188060c58ea8bc
Jack White <jack.white@kraken.tech>
2024-10-25 14:52:42 +0100

    remove nvim colorizer plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -30,21 +30,6 @@ local default_plugins = {
   },
 
   {
-    "NvChad/nvim-colorizer.lua",
-    init = function()
-      require("core.utils").lazy_load "nvim-colorizer.lua"
-    end,
-    config = function(_, opts)
-      require("colorizer").setup(opts)
-
-      -- execute colorizer as soon as possible
-      vim.defer_fn(function()
-        require("colorizer").attach_to_buffer(0)
-      end, 0)
-    end,
-  },
-
-  {
     "github/copilot.vim",
     lazy = false,
   },
