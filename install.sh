a8debc7637fc5e2dcf878965162b083ca239feb5
Jack White <jack.white@kraken.tech>
2024-11-27 12:06:45 +1100

    Remove homebrew config and add pretty printing to installation script

--- install.sh
+++ install.sh
@@ -1,65 +1,67 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 REPO_DIR=$(git rev-parse --show-toplevel)
 
-echo ""
-echo "The following prerequisites must be already be installed:"
-echo "- zsh"
-echo "- oh-my-zsh"
-echo "- starship"
-echo "- neovim"
-echo "- alacritty (macos application)"
-read -r -p "Confirm that these have been installed and continue with setup? [y/n] " input
+# Remove existing dotfile configs (clean slate)
+rm -f "$HOME/.zshrc"
+rm -f "$HOME/.config/starship.toml"
+rm -rf "$HOME/.config/alacritty"
+rm -rf "$HOME/.config/nvim"
+rm -rf "$HOME/Library/Application Support/lazygit"
 
-if [[ $input =~ ^[Yy]$ ]]; then
-  # Remove existing dotfile configs (clean slate)
-  rm -f "$HOME/.zshrc"
-  rm -f "$HOME/.config/starship.toml"
-  rm -rf "$HOME/.config/alacritty"
-  rm -rf "$HOME/.config/nvim"
-  rm -rf "$HOME/Library/Application Support/lazygit"
+# Create symlinks for dotfile configs
+print -P "%F{green}Creating symlinks for dotfile configs..."
+mkdir "$HOME/.config/alacritty"
+ln -s "$REPO_DIR/dotfiles/.zshrc" "$HOME/.zshrc"
+ln -s "$REPO_DIR/dotfiles/starship.toml" "$HOME/.config/starship.toml"
+ln -s "$REPO_DIR/dotfiles/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
+ln -s "$REPO_DIR/nvim/" "$HOME/.config/nvim"
+print -P "%F{green}Symlinks created."
 
-  # Install Homebrew
-  TEMP_DIR=$(mktemp -d)
-  HOMEBREW_URL="https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
-  echo "Installing Homebrew..."
-  curl -fsSL "$HOMEBREW_URL" -o "$TEMP_DIR/install.sh"
-  /bin/bash "$TEMP_DIR/install.sh"
-  rm -rf "$TEMP_DIR"
-  echo "Homebrew installed."
+# Install oh-my-zsh
+#TODO
 
-  # Install Homebrew apps
-  echo "Installing Homebrew apps..."
-  brew install awscli helm kubectx k8sec lazygit ripgrep tfenv tflint terraform-docs tree-sitter
-  echo "Homebrew apps installed."
-  
-  # Install Alacritty
-  echo "Installing Alacritty..."
-  git clone git@github.com:alacritty/alacritty.git
-  cd alacritty
-  make app
-  cp -r target/release/osx/Alacritty.app /Applications/
-  cd ..
-  rm -rf alacritty
-  mkdir "$HOME/.config/alacritty"
-  echo "Alacritty installed."
+# Install neovim
+#TODO
 
-  # Install Hack Nerd Font
-  echo "Installing Hack Nerd Font..."
-  cp "$REPO_DIR/fonts/Hack-Regular-Nerd-Font-Complete.ttf" "$HOME/Library/Fonts/"
-  echo "Hack Nerd Font installed."
+# Install starship
+#TODO
 
-  # Configure lazygit
-  echo "Configuring lazygit..."
-  cp -r "$REPO_DIR/lazygit" "$HOME/Library/Application\ Support/lazygit/"
+# Install Homebrew
+TEMP_DIR=$(mktemp -d)
+HOMEBREW_URL="https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
+print -P "%F{green}Installing Homebrew..."
+curl -fsSL "$HOMEBREW_URL" -o "$TEMP_DIR/install.sh"
+/bin/bash "$TEMP_DIR/install.sh"
+rm -rf "$TEMP_DIR"
+print -P "%F{green}Homebrew installed."
 
-  # Create symlinks for dotfile configs
-  ln -s "$REPO_DIR/dotfiles/.zshrc" "$HOME/.zshrc"
-  ln -s "$REPO_DIR/dotfiles/starship.toml" "$HOME/.config/starship.toml"
-  ln -s "$REPO_DIR/dotfiles/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
-  ln -s "$REPO_DIR/nvim/" "$HOME/.config/nvim"
+# Install Homebrew apps
+print -P "%F{green}Installing Homebrew apps..."
+brew install awscli helm kubectx k8sec lazygit ripgrep tfenv tflint terraform-docs tree-sitter
+print -P "%F{green}Homebrew apps installed."
 
-  # Confirm success
-  echo ""
-  echo "Success! Installation completed."
-fi
+# Install Rust (dependency for Alacritty)
+print -P "%F{green}Installing Rust..."
+curl https://sh.rustup.rs -sSf | sh
+"export PATH=$HOME/.cargo/bin:$PATH" >> "$HOME/.zshrc"
+source "$HOME/.zshrc"
+print -P "%F{green}Rust installed."
+
+# Install Alacritty
+print -P "%F{green}Installing Alacritty..."
+git clone git@github.com:alacritty/alacritty.git
+cd alacritty
+make app
+cp -r target/release/osx/Alacritty.app /Applications/
+cd ..
+rm -rf alacritty
+print -P "%F{green}Alacritty installed."
+
+# Install Hack Nerd Font
+print -P "%F{green}Installing Hack Nerd Font..."
+cp "$REPO_DIR/fonts/HackNerdFontMono-Regular-Patched.ttf" "$HOME/Library/Fonts/"
+print -P "%F{green}Hack Nerd Font installed."
+
+# Confirm success
+print -P "%F{green}\nSuccess! Installation completed."
